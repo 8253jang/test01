a5fc69858155b0d3097fb30b734de59407b6d6bd
heejung <8253jang@daum.net>
2022-08-29 19:17:19 +0900

    revert 오류 해결

--- src/kosta/mvc/view/MainView.java
+++ src/kosta/mvc/view/MainView.java
@@ -1,10 +1,9 @@
 package kosta.mvc.view;
 
-import kosta.mvc.controller.BoardController;
-
 public class MainView {
+
 	public static void main(String[] args) {
-		BoardController controller = new BoardController();
+		
 
 	}
 
