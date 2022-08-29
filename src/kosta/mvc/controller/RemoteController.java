c2ddd46b1b662f95350e85d224bd5fb62ddc423c
heejung <8253jang@daum.net>
2022-08-30 03:28:11 +0900

    local 수정

--- src/kosta/mvc/controller/RemoteController.java
+++ src/kosta/mvc/controller/RemoteController.java
@@ -1,5 +1,10 @@
 package kosta.mvc.controller;
 
 public class RemoteController {
-  
+   public void localInsert() {
+	   System.out.println("local update Çß¾î¿ä.");
+   }
+   public void select() {
+	   
+   }
 }
